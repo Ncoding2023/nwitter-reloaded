925f9c093c9bedfb382c177e27146237f3ecb7a9
Ncoding2023 <ncoding1@gmail.com>
2024-08-13 18:22:12 +0900

    2.3_3.0 Firebase Project Setup

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLfRVgjXeayd9SOtZAzyc0w0gkaMrpQ5A",
+  authDomain: "nwitter-reloaded-51a15.firebaseapp.com",
+  projectId: "nwitter-reloaded-51a15",
+  storageBucket: "nwitter-reloaded-51a15.appspot.com",
+  messagingSenderId: "499884381655",
+  appId: "1:499884381655:web:00f76ea9c5f346b789a792"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
